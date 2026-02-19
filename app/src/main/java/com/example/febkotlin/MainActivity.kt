3ea11f662c0760f41dd410b1836801f72eea2b21
Deepak <deepakcse31@gmail.com>
2026-02-19 19:24:48 +0530

    kotlin android

--- app/src/main/java/com/example/febkotlin/MainActivity.kt
+++ app/src/main/java/com/example/febkotlin/MainActivity.kt
@@ -1,16 +1,61 @@
 package com.example.febkotlin
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val userId ="1234"
+    var userName ="Dev"
+    lateinit var name: String
+
+    lateinit var edtfullname: EditText
+    lateinit var edtbranch: EditText
+    lateinit var edtenrollment: EditText
+    lateinit var edtsemester: EditText
+    lateinit var btnsubmit: Button
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        userName="DevKotlin"
+        name="DevAndroid"
+        Log.e("MainActivity", "userName: $userName")
+        Log.e("MainActivity", "name: $name")
+
+        edtfullname = findViewById(R.id.edtfullname)
+        edtbranch = findViewById(R.id.edtbranch)
+        edtenrollment = findViewById(R.id.edtenrollment)
+        edtsemester = findViewById(R.id.edtsemester)
+        btnsubmit = findViewById(R.id.btnsubmit)
+
+
+        btnsubmit.setOnClickListener {
+
+            val fullname = edtfullname.text.toString()
+            val branch = edtbranch.text.toString()
+            val enrollment = edtenrollment.text.toString()
+            val semester = edtsemester.text.toString()
+
+            Log.e("MainActivity", "fullname: $fullname")
+            Log.e("MainActivity", "branch: $branch")
+            Log.e("MainActivity", "enrollment: $enrollment")
+            Log.e("MainActivity", "semester: $semester")
+
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
